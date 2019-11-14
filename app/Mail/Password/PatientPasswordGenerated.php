c2ffd6e77a39678b0501be599b670b9a0e161b32
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2019-11-14 17:41:48 +0100

    Mise  a jour des informations des mails

---
+++ app/Mail/Password/PatientPasswordGenerated.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail\Password;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class PatientPasswordGenerated extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $user;
+    public $password;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($user,$password)
+    {
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('no-reply@medicasure.com')
+            ->subject('Medicalink App Account Information')
+            ->markdown('emails.password.patientPassowrdGenerated');
+    }
+}
--- resources/views/emails/password/patientPassowrdGenerated.blade.php
+++ resources/views/emails/password/patientPassowrdGenerated.blade.php
@@ -4,12 +4,13 @@
 Hello <strong>{{ucfirst($user->prenom).'  '.strtoupper($user->nom)}}</strong>,<br>
 Nous espérons vous trouvez bien portant. Voici <strong>vos identifiants</strong> pour accéder à votre compte Medicalink:<br>
 Email : <strong> {{$user->email}} </strong><br>
+Numero de dossier : <strong>{{$user->patient->dossier->numero_dossier}}</strong><br>
 Mot de passe : <strong>{{$password}}</strong><br><br>
 <strong><i>NB :</i> Veuillez modifier votre mot de passe à votre premiere connexion</strong>
 
 Thanks,<br>
 {{ config('app.name') }}
 <div class="div-logo-mail">
-    <img class="logo-footer" src="{{asset('/images/logo.png')}}" alt="Logo-Medicasure" height="50px" width="200px">
+<img class="logo-footer" src="{{asset('/images/logo.png')}}" alt="Logo-Medicasure" height="50px" width="200px">
 </div>
 @endcomponent
