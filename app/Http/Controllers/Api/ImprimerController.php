d4550de3438ca9dabf80daaf2135227fbe7da814
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2019-12-03 09:04:10 +0100

    Test de fonctionnement en ligne

--- app/Http/Controllers/Api/ImprimerController.php
+++ app/Http/Controllers/Api/ImprimerController.php
@@ -30,11 +30,11 @@ class ImprimerController extends Controller
     }
 
     public function generale($slug){
-        return  response()->json(['name'=>'Consultation-generale-.pdf'],419);
 
         $this->validatedSlug($slug,'consultation_medecine_generales');
 
         $consultationMedecine = ConsultationMedecineGenerale::findBySlug($slug);
+        return  response()->json(['name'=>'Consultation-generale-.pdf'],419);
 
         $data = compact('consultationMedecine');
         $pdf = PDF::loadView('rapport',$data);
