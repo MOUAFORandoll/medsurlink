e61c280185659f91ee3c6e2026426cc5c56e7757
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2019-12-26 14:23:07 +0100

    Mise a jour des mails

--- resources/views/emails/password/reset.blade.php
+++ resources/views/emails/password/reset.blade.php
@@ -1,7 +1,7 @@
 # Medicalink
 <p>You are receiving this email because we received a password reset request for your account.</p>
 @if (count($users) == 1 )
-<a href="{{url($online.'/password/reset/'.$token.'/email/'.$email)}}">Reset Password</a>
+<a href="{{url($online.'/password/reset/'.$token.'/'.$user->slug)}}">{{$user->getRoleNames()->first()}} : {{strtoupper($user->nom).' '.ucfirst($user->prenom)}}</a><br>
 @else
 <p>You have many accounts link to your Email. Please choose account you want to update</p>
 @foreach ($users as $user)
