f066f09405254f4b5b53dfc193e1582cd966eba1
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2020-01-16 09:00:54 +0100

    Mise a jour scope resultat

--- app/Scopes/RestrictResultatScope.php
+++ app/Scopes/RestrictResultatScope.php
@@ -43,6 +43,7 @@ class RestrictResultatScope implements Scope
                 $builder->whereIn('dossier_medical_id',$dossiers)->whereNotNull('archived_at');
             }elseif(gettype($userRoles->search('Medecin controle')) == 'integer'){
                 $builder->whereNotNull('passed_at');
+                $builder;
             }
             else{
                 return $builder;
