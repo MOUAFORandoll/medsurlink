8581e4856118ecc65824c8c9018392827a69af32
Erix NDEFO <tamarez4@yahoo.fr>
2020-01-27 11:20:54 +0100

    Modifications sur le rapport

--- resources/views/rapport.blade.php
+++ resources/views/rapport.blade.php
@@ -4,8 +4,49 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href='https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900&display=swap' rel='stylesheet'>
-    <link href="{{ asset('css/rapportMedical.css') }}" rel="stylesheet">
+    <!--<link href="{{ asset('css/rapportMedical.css') }}" rel="stylesheet">-->
     <title>Medicalink</title>
+
+    <style>
+            body {
+            font-size: 1em;
+            line-height: 1.2;
+            font-weight: 600;
+            font-family: "Raleway", sans-serif;
+            letter-spacing: 1.3px;
+            color: #333333;
+            background-color: white;
+        }
+
+        h2 {
+            color: #00ada7;
+            font-weight: 600;
+            text-align: left;
+            font-size: 2em !important;
+        }
+
+        h3,
+        b {
+            color: #91c01c;
+        }
+
+        td,
+        th {
+            border: 1px solid #333333;
+            padding: 1em;
+        }
+
+        table {
+            border-collapse: collapse;
+        }
+
+        .titre-rapport {
+            text-align: center;
+            text-transform: uppercase;
+        }
+
+    </style>
+
 </head>
 <body>
 
@@ -15,22 +56,21 @@
         <h3>{{$dossier->patient->date_de_naissance}}</h3>
         <h3>Sexe : {{$dossier->patient->sexe}}</h3>
     </div>
-
     <div class="justify-content-center"> Allergies Information </div>
-    @forelse($dossier->allergies as $allergie)
-        <table>
-            <thead>
-            <td>Description</td>
-            <td>Date debut</td>
-            </thead>
-            <tbody>
-            <td>{{$allergie->description}}</td>
-            <td>{{$allergie->date}}</td>
-            </tbody>
-        </table>
-    @empty
-        <p>Aucune allergie</p>
-    @endforelse
+        @forelse($dossier->allergies as $allergie)
+            <table>
+                <thead>
+                <td>Description</td>
+                <td>Date debut</td>
+                </thead>
+                <tbody>
+                <td>{{$allergie->description}}</td>
+                <td>{{$allergie->date}}</td>
+                </tbody>
+            </table>
+        @empty
+            <p>Aucune allergie</p>
+        @endforelse
 
 @endisset
 
@@ -50,7 +90,7 @@
             </div>
         @endif
 
-        <p style="text-align: center; text-transform: uppercase"><strong>Rapport de consultation</strong></p>
+        <p class="titre-rapport"><strong>Rapport de consultation</strong></p>
         <br>
         <p>Honorée Consoeur, Honorée Confrère,</p>
         <p>J'ai vue en date du <strong>{{\Carbon\Carbon::parse($consultationMedecine->date_consultation)->format('d/m/Y')}}</strong>, pour une consultation médecine générale, votre patient(e)
