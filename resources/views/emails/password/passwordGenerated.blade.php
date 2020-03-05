e3667b0ba785d8529598a338f5c3cfd499220703
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2020-03-05 15:04:25 +0100

    retrait de laravel dans les mails

--- resources/views/emails/password/passwordGenerated.blade.php
+++ resources/views/emails/password/passwordGenerated.blade.php
@@ -8,8 +8,10 @@ Mot de passe : <strong>{{$password}}</strong><br><br>
 NB : Veuillez modifier votre mot de passe à votre premiere connexion en cliquant sur <a href="https://www.medsurlink.com/login">mot de passe oublié</a>
 
 {{ config('app.name') }}
+
 <div class="div-logo-mail">
     <img class="logo-footer" src="{{asset('/images/logo.png')}}" alt="Logo-Medicasure">
 </div>
+
 <a href="{{ config('app.frontend_url') }}">Medsurlink</a>
 @endcomponent
