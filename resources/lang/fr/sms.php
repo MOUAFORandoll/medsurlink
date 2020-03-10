888a8c0b622df4f65bfc6de6b657134a449cd813
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2020-03-10 11:32:49 +0100

    retrait des restrictions liées aux questions sécrete

--- resources/lang/fr/sms.php
+++ resources/lang/fr/sms.php
@@ -2,7 +2,7 @@
 
 return [
     'accountCreated'         => 'HELLO :nom,VEUILLEZ CLIQUER SUR CE LIEN POUR ACCEDER A VOTRE COMPTE.VOTRE MOT DE PASSE SE COMPOSE DE VOTRE date de naissance SUIVI DU CODE PRIVE :password '. config('app.frontend_url'),
-    'accountSecurityUpdated' => 'HELLO :nom,POUR DES RAISONS DE SECURITE, VOTRE MOT DE PASSE A ETE MISE A JOUR.LE NOUVEAU SE COMPOSE DE VOTRE date de naissance SUIVI DU CODE PRIVE :password '. config('app.frontend_url'),
+    'accountSecurityUpdated' => 'HELLO :nom,POUR DES RAISONS DE SECURITE, VOTRE MOT DE PASSE A ETE MISE A JOUR. LE NOUVEAU SE COMPOSE DE VOTRE date de naissance SUIVI DU CODE PRIVE :password '. config('app.frontend_url'),
     'accountUpdated'         => 'HELLO :nom,VEUILLEZ CLIQUER SUR CE LIEN POUR ACCEDER A VOS INFORMATIONS MEDICAUX '. config('app.frontend_url'),
 
     ];
