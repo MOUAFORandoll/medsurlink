6c9aefb4cb5d0e748ceb268d52ea533c66a296ac
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2020-03-19 17:42:35 +0100

    Mise a jour lien dans le mail

--- resources/views/emails/setting/update.blade.php
+++ resources/views/emails/setting/update.blade.php
@@ -4,7 +4,7 @@
 Hello <strong>{{ucfirst($user->prenom).'  '.strtoupper($user->nom)}}</strong>,<br>
 <p>Vos informations ont été mise à jour avec succès</p><br>
 
-Cliquer <a href="{{ config('app.frontend_url') }}">ici</a> pour plus d'informations<br>
+<a href="{{ config('app.frontend_url') }}">Cliquer ici pour plus d'informations</a> <br>
 
 {{ config('app.name') }}
 
