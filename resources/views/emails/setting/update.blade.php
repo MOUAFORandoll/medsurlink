68700eb2abecac17f9a7f3cb14db4b13d962a3ff
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2020-03-19 17:46:48 +0100

    fix bug

--- resources/views/emails/setting/update.blade.php
+++ resources/views/emails/setting/update.blade.php
@@ -4,7 +4,7 @@
 Hello <strong>{{ucfirst($user->prenom).'  '.strtoupper($user->nom)}}</strong>,<br>
 <p>Vos informations ont été mise à jour avec succès</p><br>
 
-<a href="{{ config('app.frontend_url') }}">Cliquer ici pour plus d'informations</a> <br>
+<a href="{{ config('app.frontend_url') }}">Cliquer ici pour plus d'informations</a><br>
 
 {{ config('app.name') }}
 
