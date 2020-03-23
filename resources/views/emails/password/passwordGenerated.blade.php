27f85b2e6fbfa1bbd1e1e0cdbfaa8e0d87503e57
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2020-03-23 12:48:22 +0100

    Ajout de la possibilité à un medecin controle de modifier les consultations d'un medecin controle

--- resources/views/emails/password/passwordGenerated.blade.php
+++ resources/views/emails/password/passwordGenerated.blade.php
@@ -5,7 +5,7 @@ Bonjour <strong>{{ucfirst($user->prenom).'  '.strtoupper($user->nom)}}</strong>,
 Nous espérons vous trouver bien portant. Voici <strong>vos identifiants</strong> pour accéder à votre compte Medicalink :<br>
 Email : <strong> {{$user->email}} </strong><br>
 Mot de passe : <strong>{{$password}}</strong><br><br>
-NB : Veuillez modifier votre mot de passe à votre premiere connexion en cliquant sur <a href="https://www.medsurlink.com/login">mot de passe oublié</a>
+NB : Veuillez modifier votre mot de passe à votre premiere connexion en cliquant sur <a href="https://www.medsurlink.com/setting">paramètres</a>
 
 {{ config('app.name') }}
 
