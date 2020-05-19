8c6cca6cd6b1474a174444c175cd2ec419a90f35
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2020-05-19 16:01:15 +0100

    Mise a jour rdv

--- app/Http/Controllers/Api/RendezVousController.php
+++ app/Http/Controllers/Api/RendezVousController.php
@@ -182,7 +182,7 @@ class RendezVousController extends Controller
             ->first();
 
         defineAsAuthor("RendezVous", $rdv->id, 'update');
-
+        $rdv->updateRendezVous();
         return response()->json(['rdv'=>$rdv]);
     }
 
