e9c2545c5d1e360206b1b6b84e71388696b4c7f9
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2020-05-29 10:04:09 +0100

    Mise a jour des medecins avis dans la bd avec un seeder

---
+++ database/seeds/MedecinAvisSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MedecinAvisSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = \App\User::where('isMedicasure','1')->get();
+        foreach ($users as $user){
+            if (!is_null($user->praticien || $user->medecinControle)){
+                $user->isNotice='1';
+                $user->save();
+            }
+        }
+    }
+}
