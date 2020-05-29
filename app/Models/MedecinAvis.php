dc78b0ea6845183f33e6ce69dcde1653ccb1624b
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2020-05-29 11:42:26 +0100

    Mise en place migration et model de medecin avis

---
+++ app/Models/MedecinAvis.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use App\User;
+use Carbon\Carbon;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+
+class MedecinAvis extends Model
+{
+    use SoftDeletes;
+    use Sluggable;
+
+
+    protected $fillable = [
+        "avis_id",
+        "medecin_id",
+        "slug",
+    ];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'NameAndTimestamp'
+            ]
+        ];
+    }
+
+    public function getNameAndTimestampAttribute() {
+        return Str::random(16).' '.Carbon::now()->timestamp;
+    }
+
+    public function avis(){
+        return $this->belongsTo(Avis::class,'avis_id','id');
+    }
+
+    public function medecin(){
+        return $this->belongsTo(User::class,'medecin_id','id');
+    }
+}
