08bc80df1e038c2cfa56980a526527b7275bc830
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2020-07-21 13:12:53 +0100

    Mise a jour migration

--- database/migrations/2020_07_21_083942_add_praticien_consultation_externe_to_fichier_externes.php
+++ database/migrations/2020_07_21_083942_add_praticien_consultation_externe_to_fichier_externes.php
@@ -16,9 +16,9 @@ class AddPraticienConsultationExterneToFichierExternes extends Migration
         Schema::table('consultation_fichiers', function (Blueprint $table) {
             $table->string('praticien_externe')->nullable();
             $table->string('consultation_externe')->nullable();
-            $table->dropIndex('consultation_fichiers_user_id_foreign');
+//            $table->dropIndex('consultation_fichiers_user_id_foreign');
             $table->dropForeign('consultation_fichiers_user_id_foreign');
-            $table->unsignedBigInteger('user_id')->nullable()->change();
+            $table->string('user_id')->nullable()->change();
         });
     }
 
