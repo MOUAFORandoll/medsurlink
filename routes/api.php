c9c13d38d6032c985d0c6856b495c326773bdca7
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2020-07-22 16:51:44 +0100

    Online commit

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::get('question','Api\QuestionController@index');
 // Pour faire rapidement les tests sur suivi en back avec postman
 //Route::resource('avis','Api\AvisController');
 //Route::post('avisMedecin/{slug}','Api\AvisMedecinController@store');
-Route::resource('suivi','Api\SuiviController');
+//Route::resource('suivi','Api\SuiviController');
 
 
 Route::middleware(['auth:api'])->group(function () {
@@ -163,7 +163,7 @@ Route::group(['middleware' => ['auth:api','role:Admin|Medecin controle|Praticien
     Route::get('latest-operation','Api\AuteurController@latestOperation');
 
     Route::resource('categorie','Api\CategorieController');
-//    Route::resource('suivi','Api\SuiviController');
+    Route::resource('suivi','Api\SuiviController');
     Route::resource('toDoList','Api\ToDoListController');
     Route::post('toDoList/{slug}/statut','Api\ToDoListController@updateStatut');
     Route::resource('suivi-specialite','Api\SpecialiteSuiviController');
