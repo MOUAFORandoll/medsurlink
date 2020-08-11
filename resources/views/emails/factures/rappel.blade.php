43c26d59e59444e684c49793434bc5d3a417ff73
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2020-08-11 11:51:55 +0100

    Mise a jour mail et ajout des fichiers joints

--- resources/views/emails/factures/rappel.blade.php
+++ resources/views/emails/factures/rappel.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
-{{$souscripteur->sexe=='M' ? 'Cher Monsieur' : 'Chere Madame'}} {{strtoupper($souscripteur->user->nom).'  '.ucfirst($souscripteur->user->prenom)}} {{$souscripteur->sexe=='M' ? 'Souscripteur' : 'Souscriptrice'}}, Bonjour
+{{$souscripteur->sexe=='M' ? 'Cher Monsieur' : 'Chere Madame'}} {{strtoupper($souscripteur->user->nom).'  '.ucfirst($souscripteur->user->prenom)}} , Bonjour
 
 Sauf erreur ou omission de notre part, le paiement du solde de votre dette auprès de Medicasure ne nous est pas parvenu.
 
@@ -11,19 +11,19 @@ Si vous avez besoin d’éclaircissements ou si vous avez des questions, n'hési
 
 En vous remerciant encore pour la confiance que vous nous avez accordé, et que vous nous accorderez ultérieurement, et en espérant que vous avez été satisfait du service, nous vous prions d'agréer nos salutations cordiales.
 
-DATE LIMITE DE PAIEMENT: {{now()->addDays(7)->format('d-m-yy')}}
+DATE LIMITE DE PAIEMENT: <span style="color:red">{{now()->addDays(7)->format('d-m-yy')}}</span>
 
-COMPTABILITÉ
+___COMPTABILITÉ___
 
-Tel:+4917643171205
-+237674411042
-comptabilite@medicasure.com
+___Tel:+4917643171205___<br>
+___+237674411042___<br>
+___comptabilite@medicasure.com___<br>
 
-Intermédiation médicale
+___INTERMÉDIATION MÉDICALE___
 
-Rue du Castel 18, 7801 Ath Belgique
-Face Lycée de Ndogpassi Douala Cameroun
-www.medicasure.com
+___Rue du Castel 18, 7801 Ath Belgique___<br>
+___Face Lycée de Ndogpassi Douala Cameroun___<br>
+___https://www.medicasure.com___<br>
     <div class="div-logo-mail">
         <img class="logo-footer" src="{{asset('/images/logo.png')}}" alt="Logo-Medicasure">
     </div>
