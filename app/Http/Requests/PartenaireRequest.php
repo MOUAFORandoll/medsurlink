1e2616ffba47ea4989c2c62387f623e4ed9a1573
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2020-12-09 14:42:10 +0100

    Mise en place model et request partenaires

---
+++ app/Http/Requests/PartenaireRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PartenaireRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "region"=>"sometimes|nullable|string",
+            "ville"=>"sometimes|nullable|string",
+            "nom"=>"sometimes|nullable|string",
+            "localisation"=>"sometimes|nullable|string",
+            "telephone"=>"sometimes|nullable|string",
+            "email"=>"sometimes|nullable|string|email",
+            "contact"=>"sometimes|nullable|string",
+        ];
+    }
+}
