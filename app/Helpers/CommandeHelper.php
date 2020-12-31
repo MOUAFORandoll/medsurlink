0c5252f0093cea5394657f9b034b8fb48c4fa584
Wilfried NKOUEKAM MBOUGA <nkouekamMedicasure@bitbucket.org>
2020-12-31 13:16:07 +0100

    retrait date signature et montant souscription dans les parametres de la requete de creation de patients

--- app/Helpers/CommandeHelper.php
+++ app/Helpers/CommandeHelper.php
@@ -67,8 +67,8 @@ if(!function_exists('transformerCommande')) {
         $detailContrat['nom_pere']=$request->nom_pere;
         $detailContrat['nom_mere']=$request->nom_mere;
         $detailContrat['canal']=$request->canal;
-        $detailContrat['dateSignature']=$request->date_signature;
-        $detailContrat['montantSouscription']=$request->montant_souscription;
+        $detailContrat['dateSignature']=$request->date_paiement;
+        $detailContrat['montantSouscription']=$commande->montant;
         $detailContrat['paye_par_affilie']='non';
         $detailContrat['renouvelle']='non';
         $detailContrat['decede']='non';
