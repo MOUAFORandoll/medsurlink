fc2cafb8eb8a4e9ca721ba89d517f5337c779b5f
Daniel ngantchou <ngantchou@medicasure.com>
2021-08-27 11:19:43 +0100

    fusion des deux tableaux examens complémentaires

--- app/Http/Controllers/Api/ExamenComplementaireController.php
+++ app/Http/Controllers/Api/ExamenComplementaireController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 
+use App\Models\OtherComplementaire;
 use App\Http\Controllers\Controller;
 use App\Models\ExamenComplementaire;
 
 class ExamenComplementaireController extends Controller
 {
     public function index()
-    {
-        $examencomplementaire = ExamenComplementaire::get();
-        return response()->json(['examencomplementaire'=>$examencomplementaire]);
+    {    
+        $othercomplementaire = OtherComplementaire::get()->toArray();
+        $examencomplementaire = ExamenComplementaire::get()->toArray();
+        $fusion = array_merge($othercomplementaire,$examencomplementaire);
+        return response()->json([
+            'examencomplementaire'=>$fusion
+        ]);
     }
 }
