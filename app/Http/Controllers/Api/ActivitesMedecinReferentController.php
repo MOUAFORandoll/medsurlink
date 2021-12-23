b7b53b41b447bd53278361ac467be9419edc82d8
Stephen king <jacquesstephan10@gmail.com>
2021-12-23 14:23:20 +0100

    Activité medecin referent model controller et seeder

---
+++ app/Http/Controllers/Api/ActivitesMedecinReferentController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\ActivitesMedecinReferent;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ActivitesMedecinReferentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $medref = ActivitesMedecinReferent::with('etablissements','user')->get();
+        return  response()->json([
+            'pec' => $medref,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ActivitesMedecinReferent  $activitesMedecinReferent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ActivitesMedecinReferent $activitesMedecinReferent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ActivitesMedecinReferent  $activitesMedecinReferent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ActivitesMedecinReferent $activitesMedecinReferent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ActivitesMedecinReferent  $activitesMedecinReferent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ActivitesMedecinReferent $activitesMedecinReferent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ActivitesMedecinReferent  $activitesMedecinReferent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ActivitesMedecinReferent $activitesMedecinReferent)
+    {
+        //
+    }
+}
---
+++ app/Models/ActivitesMedecinReferent.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ActivitesMedecinReferent extends Model
+{
+    use Sluggable;
+    use SoftDeletes;
+
+    protected $table = 'activites_medecin_referent';
+    protected $fillable = [
+        "fr_description",
+        "en_description",
+        "slug",
+    ];
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'DossierAndTimestamp'
+            ]
+        ];
+    }
+
+    public function getDossierAndTimestampAttribute() {
+        return Str::random(10) . ' ' .Carbon::now()->timestamp;
+    }
+}
