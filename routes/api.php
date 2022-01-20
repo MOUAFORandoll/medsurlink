9d6c9df5761ec831bdb2324e823faeb7af5bc97e
ngantchou joel daniel <ngantchou@medicasure.com>
2022-01-20 12:04:58 +0000

    api.php edited online with Bitbucket

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 use Psy\Util\Json;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-/**-- Headers --**/
 
 header('Access-Control-Allow-Origin:  *');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE, PATCH');
