9b44331304d879635990cc439b394aced9dc5794
Berthold Feujo <feujio@medicasure.com>
2022-02-25 17:46:13 +0100

    modification de l'url de redirection

--- app/Http/Controllers/Api/StripeContrtoller.php
+++ app/Http/Controllers/Api/StripeContrtoller.php
@@ -259,7 +259,7 @@ class StripeContrtoller extends Controller
            }
 
            if($token=="checkout"){
-            $updatePath = 'checkout';
+            $updatePath = 'success';
            }else{
             $updatePath = 'contrat-prepaye/add?status=success&token='.$token;
            }
