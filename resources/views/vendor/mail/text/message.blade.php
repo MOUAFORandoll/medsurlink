90927da73d058b404f1be01037e5834862e18b91
Mbida Stephan <mbida@medicasure.com>
2022-03-10 11:18:01 +0100

    modification des emails

--- resources/views/vendor/mail/text/message.blade.php
+++ resources/views/vendor/mail/text/message.blade.php
@@ -21,7 +21,7 @@
     {{-- Footer --}}
     @slot('footer')
         @component('mail::footer')
-            © {{ date('Y') }} <a class="lien-footer" href="www.medicasure.com">Medicasure</a> @lang('All rights reserved.')
+            © {{ date('Y') }} <a class="lien-footer" href="www.medicasure.com">Medsurlink by Medicasure</a> @lang('Tous droits réservés.')
         @endcomponent
     @endslot
 @endcomponent
