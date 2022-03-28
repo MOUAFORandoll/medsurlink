8060e4452a201baef224a08cedaa024a4f4999a4
Berthold Feujo <feujio@medicasure.com>
2022-03-28 17:44:55 +0100

    Intagration de OM et envoie des mails au souscripteur

--- app/Helpers/GeneralHelper.php
+++ app/Helpers/GeneralHelper.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Mail\Facture\AchatOffre;
+use App\Mail\Facture\PaiementPrestation;
 use App\Models\Affiliation;
 use App\Models\AffiliationSouscripteur;
 use App\SMS;
@@ -183,7 +184,6 @@ if(!function_exists('formatTelephone'))
         }
     }
 }
-
 if(!function_exists('generationPdfFactureOffre'))
 {
     /**
@@ -195,6 +195,44 @@ if(!function_exists('generationPdfFactureOffre'))
         //return view('impression_offre');
         try {
             $pdf = PDF::loadView('impression_offre', ['commande_id' => $commande_id, 'commande_date' => $commande_date, 'montant_total' => number_format($montant_total, 2, ',', ' '), 'echeance' => $echeance, 'description' => mb_strtoupper($description), 'quantite' => $quantite, 'prix_unitaire' => number_format($prix_unitaire, 2, ',', ' '), 'nom_souscripteur' => $nom_souscripteur, 'email_souscripteur' => $email_souscripteur, 'rue' => $rue, 'adresse' => $adresse, 'ville' => $ville, 'pays' => $pays, 'beneficiaire' => $beneficiaire]);
+            return ['output' => $pdf->output(), 'stream' => $pdf->stream($description.".pdf")];
+        }catch (\Exception $exception){
+            //$exception
+        }
+    }
+}
+
+
+if(!function_exists('EnvoieDeFactureApresPaiementPrestation'))
+{
+    /**
+     * @param $string_with_accent
+     * @return string
+     */
+    function EnvoieDeFactureApresPaiementPrestation($commande_id, $commande_date, $montant_total, $echeance, $description, $mode_paiement, $nom_souscripteur, $email_souscripteur, $rue, $adresse, $ville, $pays, $beneficiaire)
+    {
+        //return view('impression_offre');
+        try {
+            $pdf = generationPdfPaiementPrestation($commande_id, $commande_date, $montant_total, $echeance, $description, $mode_paiement, $nom_souscripteur, $email_souscripteur, $rue, $adresse, $ville, $pays, $beneficiaire);
+            Mail::to($email_souscripteur)->send(new PaiementPrestation($pdf['output'], $nom_souscripteur, $beneficiaire, $description));
+        }catch (\Exception $exception){
+            //$exception
+        }
+    }
+}
+
+
+if(!function_exists('generationPdfPaiementPrestation'))
+{
+    /**
+     * @param $string_with_accent
+     * @return string
+     */
+    function generationPdfPaiementPrestation($commande_id, $commande_date, $montant_total, $echeance, $description, $mode_paiement, $nom_souscripteur, $email_souscripteur, $rue, $adresse, $ville, $pays, $beneficiaire)
+    {
+        //return view('impression_offre');
+        try {
+            $pdf = PDF::loadView('impression_prestation', ['commande_id' => $commande_id, 'commande_date' => $commande_date, 'montant_total' => number_format($montant_total, 2, ',', ' '), 'echeance' => $echeance, 'description' => mb_strtoupper($description), 'mode_paiement' => $mode_paiement, 'nom_souscripteur' => $nom_souscripteur, 'email_souscripteur' => $email_souscripteur, 'rue' => $rue, 'adresse' => $adresse, 'ville' => $ville, 'pays' => $pays, 'beneficiaire' => $beneficiaire]);
             return ['output' => $pdf->output(), 'stream' => $pdf->stream($description.".pdf")];
         }catch (\Exception $exception){
             //$exception
---
+++ app/Mail/Facture/PaiementPrestation.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail\Facture;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class PaiementPrestation extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $pdf, $user, $patient, $prestation;
+
+    public function __construct($pdf, $user, $patient, $prestation ="actes médicaux")
+    {
+        $this->pdf = $pdf;
+        $this->user = $user;
+        $this->patient = $patient;
+        $this->prestation = $prestation;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('no-reply@medsurlink.com')->subject("Reçu de $this->prestation")
+                ->markdown('emails.Souscripteur.paiementPrestation')
+                ->attachData($this->pdf,"$this->prestation du ".date('d-m-Y').".pdf");
+    }
+}
