b8e1d924ed537415c0c6bc3fe0979ec01304fbb6
Mbida Stephan <mbida@medicasure.com>
2022-04-07 10:38:41 +0100

    configuration compte pharmacien

--- app/Http/Controllers/Api/EtablissementExerciceController.php
+++ app/Http/Controllers/Api/EtablissementExerciceController.php
@@ -17,6 +17,7 @@ use App\Models\EtablissementExercicePatient;
 use App\Models\EtablissementExercicePraticien;
 use App\Http\Controllers\Traits\PersonnalErrors;
 use App\Http\Requests\EtablissementExerciceRequest;
+use App\Models\Pharmacien;
 use Netpok\Database\Support\DeleteRestrictionException;
 
 /**
@@ -243,6 +244,12 @@ class EtablissementExerciceController extends Controller
             return response()->json(['etablissements'=>$etablissements]);
 
         }
+        else if(gettype($userRoles->search('Pharmacien')) == 'integer'){
+            $etablissements = EtablissementExercice::with(['','comptables.user','pharmaciens.user','patients.user','patients.dossier','prestations.prestation','factures.dossier.patient.user'])->latest()->get();
+
+            return response()->json(['etablissements'=>$etablissements]);
+
+        }
         else if(gettype($userRoles->search('Souscripteur')) == 'integer'){
             $user = \App\User::whereId(Auth::id())->first();
             $patients = $user->souscripteur->patients;
@@ -269,6 +276,7 @@ class EtablissementExerciceController extends Controller
             $user = Auth::user();
             $comptables = Comptable::where('user_id',$user->id)->latest()->get();
             $assistantes = Assistante::where('user_id',$user->id)->latest()->get();
+            $pharmaciens = Pharmacien::where('user_id',$user->id)->latest()->get();
 
             $etablissementsId = [];
 
@@ -278,6 +286,9 @@ class EtablissementExerciceController extends Controller
             foreach ($assistantes as $assistante){
                 array_push($etablissementsId,$assistante->etablissement_id);
             }
+            foreach ($pharmaciens as $pharmacien){
+                array_push($etablissementsId,$pharmacien->etablissement_id);
+            }
 
 
 
@@ -286,6 +297,8 @@ class EtablissementExerciceController extends Controller
                 'comptables.user',
                 'assistantes',
                 'assistantes.user',
+                'pharmaciens',
+                'pharmaciens.user',
                 'patients',
                 'patients.user',
                 'patients.dossier',
