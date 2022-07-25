0aca87381f5fda3319d398a845fc5b6ab77e4952
Berthold FEUJO <feujio@medicasure.com>
2022-07-25 15:21:37 +0100

    Correction de l'affichage des validation coté médecin controle

--- app/Http/Controllers/Api/ConsultationExamenValidationController.php
+++ app/Http/Controllers/Api/ConsultationExamenValidationController.php
@@ -33,8 +33,9 @@ class ConsultationExamenValidationController extends Controller
         $examen_validation = ConsultationExamenValidation::has('consultation')->with(['consultation.ligneDeTemps.motif','consultation.dossier.patient.user','consultation.author'])
         //->whereNull('etat_validation_medecin')
         ->distinct()
+        ->latest()
         ->get(['consultation_general_id']);
-        $examen_validation = $examen_validation->sortByDesc('updated_at');
+        //$examen_validation = $examen_validation->sortByDesc('updated_at');
         return response()->json(['examen_validation'=>$examen_validation]);
     }
     /**
