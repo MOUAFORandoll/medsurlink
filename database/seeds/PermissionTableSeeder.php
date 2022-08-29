518bc1914ba887d233b9b79fab195af57779f1c1
Yolande MOBOU <mobou@medicasure.com>
2022-08-29 20:25:44 +0200

    classify Role et permission

--- database/seeds/PermissionTableSeeder.php
+++ database/seeds/PermissionTableSeeder.php
@@ -15,85 +15,115 @@ class PermissionTableSeeder extends Seeder
     public function run()
     {
        $permissions = [
+        //role pour superAdmin
            'Super Administrer les roles & permissions',
            'Administrer les roles & permissions',
+        //Patient
            'Consulter profils patient',
+           'Creer patients',
+           'Modifier patients',
+           'Supprimer patients',
+           'Consulter patients',
+        //Consultations archivees
            'Consulter consultations archivees',
-           'Consulter resultats laboratoire archives',
-           'Consulter resultats imagerie archives',
-           'Imprimer rapports medicaux',
-           'Imprimer resultats laboratoire',
-           'Imprimer resultats imagerie',
-           'Consulter profils souscripteur',
+        //Consultations
            'Imprimer consultations',
-           'Consulter profils praticien',
            'Consulter consultations',
            'Creer consultations',
            'Modifier consultations',
            'Supprimer consultations',
            'Transmettre consultation',
+           'Consulter consultations transmises',
+           'Archiver consultations transmises',   
+        //Rapports
+           'Imprimer rapports medicaux',
+        //profils souscripteur 
+           'Consulter profils souscripteur',
+         //profils praticien   
+           'Consulter profils praticien',
+        //Resultats laboratoire
            'Consulter resultats laboratoire',
            'Creer résultats laboratoire',
            'Modifier resultats laboratoire',
            'Supprimer resultats laboratoire',
-           'Transmettre resultats laboratoire',
+           'Transmettre resultats laboratoire', 
+           'Consulter resultats laboratoire archives',  
+           'Imprimer resultats laboratoire',       
+           'Consulter resultats laboratoire transmises',  
+           'Archiver resultats laboratoire transmises',
+        //Resultats imagerie
            'Consulter resultats imagerie',
            'Creer resultats imagerie',
            'Modifier resultats imagerie',
            'Supprimer  resultats imagerie',
            'Transmettre résultats imagerie',
            'Imprimer résultats imagerie',
-           'Consulter profils medecin controle',
-           'Consulter consultations transmises',
-           'Consulter resultats laboratoire transmises',
+           'Consulter resultats imagerie archives', 
+           'Imprimer resultats imagerie',
            'Consulter resultats imagerie transmises',
-           'Archiver consultations transmises',
-           'Archiver resultats laboratoire transmises',
            'Archiver resultats imagerie transmises',
+        //Profils medecin controle
+           'Consulter profils medecin controle',
+          
+        //Profils profils utilisateurs 
            'Creer profils utilisateurs',
            'Consulter profils utilisateurs',
            'Modifier profils utilisateurs',
            'Desactiver  profils utilisateurs',
+        //Profils dossier médical
            'Consulter dossier médical',
            'Creer  dossier médical',
            'Modifier dossier médical',
+        //Profils souscriptions affiliations
            'Consulter souscriptions affiliations',
            'Creer souscriptions affiliations',
            'Modifier souscriptions affiliations',
+        //Rendez-vous
            'Supprimer  rendez-vous',
            'Consulter souscriptions rendez-vous',
            'Creer souscriptions rendez-vous',
            'Modifier souscriptions rendez-vous',
-           
+        //e-prescriptions   
            'Supprimer  e-prescriptions',
            'Consulter souscriptions e-prescriptions',
            'Creer souscriptions e-prescriptions',
            'Modifier souscriptions e-prescriptions',
+        //Téléconsultation
            'Supprimer Téléconsultation',
            'Consulter Téléconsultation',
            'Creer Téléconsultation',
            'Modifier Téléconsultation',
+        //Monitoring des paramètres
            'Monitoring des paramètres',
+        //Recommandation
            'Supprimer  Recommandation',
            'Creer  Recommandation',
            'Consulter  Recommandation',
            'Modifier  Recommandation',
+        //Chat
            'Accéder au Chat',
+        //Parcours de soins
            'Accéder au Parcours de soins',
+        //Géolocalisation
            'Géolocalisez',
+        //souscriptions assureurs
            'Creer souscriptions assureurs',
            'Modifier souscriptions assureurs',
            'Desactiver souscriptions assureurs',
+        //validation
            'Effectuer validation financieres',
            'Effectuer validation médical',
+        //Formations
            'Creer formations',
            'Modifier formations',
            'Supprimer formations',
            'Consulter formations',
+        //Avis-medicaux
            'Creer avis-medicaux',
            'Modifier avis-medicaux',
            'Supprimer avis-medicaux',
            'Consulter avis-medicaux',
+        //Comptable
            'Creer comptable',
            'Modifier comptable',
            'Supprimer comptable',
@@ -101,18 +131,17 @@ class PermissionTableSeeder extends Seeder
            'Creer souscripteur',
            'Modifier souscripteur',
            'Supprimer souscripteur',
-           'Creer patients',
-           'Modifier patients',
-           'Supprimer patients',
-           'Consulter patients',
+        //Partenaires   
            'Creer partenaires',
            'Modifier partenaires',
            'Supprimer partenaires',
            'Consulter partenaires',
+        //Association
            'Creer association',
            'Modifier association',
            'Supprimer association',
            'Consulter association',
+        //Activite
            'Creer activite',
            'Modifier activite',
            'Consulter activite patients',
