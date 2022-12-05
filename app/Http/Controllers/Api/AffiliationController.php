43df6a3e7a07f58388874cbb241c81ea7db74afb
Mbida Stephan <mbida@medicasure.com>
2022-12-05 11:19:29 +0100

    changement de la vérification

--- app/Http/Controllers/Api/AffiliationController.php
+++ app/Http/Controllers/Api/AffiliationController.php
@@ -411,11 +411,12 @@ class AffiliationController extends Controller
 
         //Ici on determine si le patient a deja une affiliation pour cette année
         $affiliation =  Affiliation::where('patient_id','=',$request->patient_id)->where('package_id', $request->package_id)->where('nom','=','Annuelle')->WhereYear('date_debut',$date_debut)->latest()->get();
-
-        if ($affiliation[0]->package->offre_id != 1) {
-            $msg = $affiliation[0]->package;
-            $message = "Le patient dispose déjà de la même affiliation pour cette année ({$msg->description_fr})";
-            $this->revealError('dejaAffilie',$message);
+        if (count($affiliation)>0) {
+            if ($affiliation[0]->package->offre_id != 1) {
+                $msg = $affiliation[0]->package;
+                $message = "Le patient dispose déjà de la même affiliation pour cette année ({$msg->description_fr})";
+                $this->revealError('dejaAffilie',$message);
+            }
 
         } elseif ($request->nom == "One shot"){
             //On determine si le patient a deja une affiliation oneshot a ce jour
