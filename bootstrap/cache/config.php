2edbf613aa4740be46a33af0d90c512a441d9f8d
Berthold FEUJO <feujio@medicasure.com>
2023-03-29 10:58:37 +0100

    Merge branch 'feature/teleconsultation' of bitbucket.org:ericndefo/medicalink-app into feature/staging

--- bootstrap/cache/config.php
+++ bootstrap/cache/config.php
@@ -1032,7 +1032,7 @@
     ),
     'teleconsultations' => 
     array (
-      'base_uri' => 'http://localhost:8002',
+      'base_uri' => 'http://staging-back-teleconsultations.medsurlink.com',
       'secret' => 'EgDwYss1HthxUbAjbRViO0QaNF82gsJIyCiKXiZr',
     ),
   ),
