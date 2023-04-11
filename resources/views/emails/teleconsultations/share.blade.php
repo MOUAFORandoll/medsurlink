24c6bb75553d95680abe263ebd2f0b972d9dbe34
Berthold FEUJO <feujio@medicasure.com>
2023-04-11 17:08:06 +0100

    Envoie du bon de prise en charge par email

--- resources/views/emails/teleconsultations/share.blade.php
+++ resources/views/emails/teleconsultations/share.blade.php
@@ -1,18 +1,23 @@
-<html>
-    <head>
-        <title>
-            {{ $subject }}
-        </title>
-    </head>
-    <body>
-        <div> {{ $contenu }} </div>
-        <div>
-            {{ $route }}
+@extends('emails.layouts.app')
+@section('title', $subject)
+@section('content')
+    {{-- <div class="content-title">
+        <h1>
+            Votre <span>santé</span> est notre priorité
+        </h1>
+        <div class="img">
+            <img src="{{ asset('images/emails/man.png') }}" alt="">
         </div>
-
-        Cliquez sur le lien ci-dessous
-        <div>
-             <a href="{{ $route }}">{{ $route }}</a>
-         </div>
-    </body>
-</html>
+    </div> --}}
+    <div class="content-body">
+        <h2>Hello,</h2>
+        <div class="content-body-text">
+            <p>{{ $contenu }}</p>
+           {{--  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias veniam sint debitis quo harum possimus fugiat expedita velit. Soluta magni distinctio fugiat error, maxime velit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus modi necessitatibus atque similique eius commodi delectus a quo, consequuntur cupiditate obcaecati vero maiores quos veniam?</p> --}}
+        </div>
+        <p class="gray-color">- {{ config('app.name') }}</p>
+        <div class="text-center">
+            <a href="{{ $route }}">Accéder au bon de prise en charge</a>
+        </div>
+    </div>
+@endsection
--- resources/views/pdf/includes/identification_patient.blade.php
+++ resources/views/pdf/includes/identification_patient.blade.php
@@ -6,25 +6,25 @@ use Carbon\Carbon;
     <ul class="list-none">
         <li class="li-top">
             <span>
-                <span class="title">Nom: </span>
+                <span class="title">Nom : </span>
                 <span class="info"> {{ $patient->user->name }}</span>
             </span>
             <span>
-                <span class="title">Age: </span>
+                <span class="title">Age : </span>
                 <span class="info">{{ $patient->age }}</span>
             </span>
             <span>
-                <span class="title">Sexe: </span>
+                <span class="title">Sexe : </span>
                 <span class="info">{{ $patient->sexe == "M" ? "Masculin" : "Féminin" }}</span>
             </span>
         </li>
         <li class="li-bottom">
             <span>
-                <span class="title">Date de naissance: </span>
+                <span class="title">Date de naissance : </span>
                 <span class="info"> {{ Carbon::parse($patient->date_de_naissance)->locale(config('app.locale'))->translatedFormat('jS F Y') }} </span>
             </span>
             <span>
-                <span class="title">Numéro de dossier: </span>
+                <span class="title">Numéro de dossier : </span>
                 <span class="info">{{ $patient->dossier->numero_dossier }}</span>
             </span>
         </li>
