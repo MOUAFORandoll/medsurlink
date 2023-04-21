6fae7335ae50c9128572e8541a0642c60e6da257
Mbida Stephan <mbida@medicasure.com>
2023-04-21 09:06:51 +0100

    Assignation d'une permission a un role operationneléé

---
+++ app/Models/Permission.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    protected $fillable = [
+        "name",
+        "guard_name",
+    ];
+
+    protected $guard_name = 'web';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id')->withTrashed();
+    }
+}
