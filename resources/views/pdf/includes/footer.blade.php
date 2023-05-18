25c61a7c48feeba97cefc72074f3969972169a86
Berthold FEUJO <feujio@medicasure.com>
2023-05-18 15:19:06 +0100

    Correction du pied de page du pdf

--- resources/views/pdf/includes/footer.blade.php
+++ resources/views/pdf/includes/footer.blade.php
@@ -6,26 +6,26 @@
     <div id="pageNumbers" style="float: right; color: #fff; margin-right: 2rem; font-size:0.5rem;">
         <div class="page-number"></div>
     </div>
-    <h4 class="text-left p-0 m-0 white" style="{{ $format == 'a6' ? 'margin-left: 2rem; font-size:0.5rem' : 'margin-left: 2rem; font-size:0.8rem' }};">Contact Medsurlink </h4>
+    <h4 class="text-left p-0 m-0 white" style="{{ $format == 'a6' ? 'margin-left: 2rem; font-size:0.43rem' : 'margin-left: 2rem; font-size:0.8rem' }};">Contact Medsurlink </h4>
     <ul class="list-none" style="margin-left: 2rem">
         <li class="li-top text-left">
             <span>
-                <span class="title">Tél :</span>
-                <span class="info">0032 491 64 64 14</span>
+                <span class="title" style="{{ $format == 'a6' ? 'font-size:0.43rem': '' }};">Tél :</span>
+                <span class="info" style="{{ $format == 'a6' ? 'font-size:0.43rem': '' }};">0032 491 64 64 14</span>
             </span>
             <span>
-                <span class="title">Email :</span>
-                <span class="info">contact@medsurlink.com</span>
+                <span class="title" style="{{ $format == 'a6' ? 'font-size:0.43rem': '' }};">Email :</span>
+                <span class="info" style="{{ $format == 'a6' ? 'font-size:0.43rem': '' }};">contact@medsurlink.com</span>
             </span>
             <span>
-                <span class="title">Site web :</span>
-                <span class="info">www.medicasure.com</span>
+                <span class="title" style="{{ $format == 'a6' ? 'font-size:0.43rem': '' }};">Site web :</span>
+                <span class="info" style="{{ $format == 'a6' ? 'font-size:0.43rem': '' }};">www.medicasure.com</span>
             </span>
         </li><br />
         <li class="text-left">
             <span>
-                <span class="title">Adresse :</span>
-                <span class="info">Rue du castel(Irc) 18-7801 Ath Belgique</span>
+                <span class="title" style="{{ $format == 'a6' ? 'font-size:0.43rem': '' }};">Adresse :</span>
+                <span class="info" style="{{ $format == 'a6' ? 'font-size:0.43rem': '' }};">Rue du castel(Irc) 18-7801 Ath Belgique</span>
             </span>
         </li>
     </ul>
